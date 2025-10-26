46b5af976dd2a91c3b7a7707840ca5b03c79723f
josigar <104169012+josigar123@users.noreply.github.com>
2025-10-26 12:22:41 +0100

    Fix: encapsulation

--- src/p2p/FingerTableEntry.java
+++ src/p2p/FingerTableEntry.java
@@ -2,9 +2,9 @@ package p2p;
 
 public class FingerTableEntry {
 
-    int start;
-    int end;
-    NodeInterface successor;
+    public int start;
+    public int end;
+    public NodeInterface successor;
 
     public FingerTableEntry(int start, int end, NodeInterface successor) {
         this.start = start;
